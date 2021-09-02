f83953917b184dc4d292ebd60e09baac4ca3f646
Michael-Nurse-Dev <68805394+Michael-Nurse-Dev@users.noreply.github.com>
2021-09-02 16:43:18 +0200

    Landing page done

--- js/site.js
+++ js/site.js
@@ -1,4 +1,53 @@
-//Define Hello World
-function HelloWorld(){
-   alert("Hello World");
+function getWordsEntered(){
+   const originalWords = document.getElementById("wordsEnteredId").value;
+   
+   const reversed = reverseWords(originalWords);
+   displayMoontalk(reversed);
 }
+
+
+function reverseWords(words){
+
+   wordiness = words.length;
+
+   reversed = [];
+
+   for (let i = 0; i < wordiness; i++) {
+      const element = words[wordiness - i -1];
+      reversed.push(element);
+   }
+
+   return reversed;
+}
+
+// //UI function
+function displayMoontalk(moontalking){
+
+   const html = moontalking.join("");
+
+   document.getElementById("results").innerHTML = html;
+}
+
+
+
+// function displayNumbers(numbers){
+
+//    let templateRows = "";
+
+//    for (let i = 0; i < numbers.length; i++) {
+//       let n = numbers[i];
+//       let className = "";
+
+//       if (n % 2 == 0) {
+//          className = "even";
+//       }
+//       else{
+//          className = "odd";         
+//       }
+
+//       templateRows += `<tr><td class="${className}">${n}</td></tr>`;      
+//    }
+
+//    // Insert the final result into the DOM.
+//    document.getElementById("results").innerHTML = templateRows;
+// }
